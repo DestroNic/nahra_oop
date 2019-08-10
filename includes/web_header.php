1c29f05ef586b979ebed747cc18e6e885c674328
Destronic <rcmalonso@gmail.com>
2019-08-09 19:15:07 -0700

    first commit

---
+++ includes/web_header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Red+Hat+Display&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="assets/main.css">
+	<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+	<script type="text/javascript" src="assets/jquery.js"></script>
+</head>
+<body>
+<div class="header">
+  <h1>NAHRA  </h1>
+  <p>Nicaraguan American Human Rights Alliance</p>
+</div>
+<div class="navbar">
+  <ul>
+    <li><a href="index.php">Inicio</a></li>
+    <li><a href="about.php">Acerca de Nosotros</a></li>
+    <li><a href="info.php">Informacion</a></li>
+    <li><a href="services.php">Servicios</a></li>
+    <li><a href="resources.php">Recursos</a></li>
+    <li><a href="contactus.php">Contactanos</a></li>
+  </ul>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+include('includes/web_header.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>NAHRA - Inicio</title>
+</head>
+<body>
+<div id="slideshow">
+	<div>
+		<img src="images/pic1.jpg" height="600px" width="1200px">
+	</div>
+	<div>
+		<img src="images/pic2.jpg" height="600px" width="1200px">
+	</div>
+	<div>
+		<img src="images/pic3.jpg" height="600px" width="1200px">
+	</div>
+	<div>
+		<img src="images/pic4.jpg" height="600px" width="1200px">
+	</div>
+	<div>
+		<img src="images/pic5.jpg" height="600px" width="1200px">
+	</div>
+</div>
+
+</body>
+</html>
